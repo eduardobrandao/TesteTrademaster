308a3957d9b41cbe0d13f4fd4600c678665707ed
EduardoBrandao <eduardo.loureiro@madeinweb.app>
2025-06-23 11:01:00 -0300

    ajustando script de teste adicionando o cenário do Observer/eventEmiter

--- sendMessage.js
+++ sendMessage.js
@@ -1,4 +1,10 @@
 const amqp = require("amqplib");
+const { eventBus } = require("./src/patterns/eventBus");
+
+
+eventBus.on("eventProcessed", (event) => {
+  console.log("🔔 Evento processado via Observer (eventBus):", event);
+});
 
 (async () => {
   const conn = await amqp.connect("amqp://rabbitmq:5672");
@@ -9,18 +15,19 @@ const amqp = require("amqplib");
 
   const orderMessage = {
     type: "ORDER_CREATED",
-    data: { orderId: Math.floor(Math.random() * 1000) }
+    data: { orderId: Math.floor(Math.random() * 1000) },
   };
 
   const paymentMessage = {
     type: "PAYMENT_RECEIVED",
-    data: { paymentId: Math.floor(Math.random() * 1000) }
+    data: { paymentId: Math.floor(Math.random() * 1000) },
   };
 
   channel.sendToQueue(queue, Buffer.from(JSON.stringify(orderMessage)));
   channel.sendToQueue(queue, Buffer.from(JSON.stringify(paymentMessage)));
 
-  console.log("Mensagens enviadas!");
+  console.log("📨 Mensagens enviadas para o RabbitMQ!");
+
   await channel.close();
   await conn.close();
 })();
